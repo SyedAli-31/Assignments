56c135d0659428764578fe6eaf2f9a32a80179a5
SyedAli-31 <164929736+SyedAli-31@users.noreply.github.com>
2024-05-19 23:44:43 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+/////Ticket checker Assignment////////////
+var customerage = 61;
+if (customerage <= 5) {
+    console.log("Welcome your tivke is free");
+}
+else if (customerage <= 20) {
+    console.log("You are an adult, Your ticket price is 200 ");
+}
+else if (customerage >= 60) {
+    console.log("Welcome your are old citien,you have 50% DISCOUNT on your ticket price ");
+}
+else {
+    console.log("Park is closed");
+}
